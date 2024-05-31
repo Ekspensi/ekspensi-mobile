2c763fa2d248fbac8dd7ded855caf1487f64a12d
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-05-31 10:44:05 +0700

    refactor for cleaner folder structure

--- app/src/main/java/com/bangkit/application/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/application/view/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.bangkit.application
+package com.bangkit.application.view.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bangkit.application.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
