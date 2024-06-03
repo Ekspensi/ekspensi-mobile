05ed755725bd7c7688043d683b1b619c8c98e1a1
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-03 19:27:56 +0700

    finishing signup page styling

--- app/src/main/java/com/bangkit/application/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/application/view/main/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.bangkit.application.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.application.R
+import com.bangkit.application.view.signup.SignupActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, SignupActivity::class.java))
+
     }
+
+
 }
--- app/src/main/java/com/bangkit/application/view/signup/SignupActivity.kt
+++ app/src/main/java/com/bangkit/application/view/signup/SignupActivity.kt
@@ -1,15 +1,41 @@
 package com.bangkit.application.view.signup
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.application.R
+import com.bangkit.application.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction(){
+
     }
 }
