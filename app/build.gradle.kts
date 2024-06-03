31143d18302761c92206f150d175c5a4929366e2
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-03 19:45:32 +0700

    adding retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
 }
---
+++ app/src/main/java/com/bangkit/application/data/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.bangkit.application.data
+
+class UserRepository {
+}
