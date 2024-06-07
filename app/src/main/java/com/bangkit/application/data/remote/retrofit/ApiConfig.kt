ba7717bae0d6f1fdf48cc437f0d9bd45111e82ac
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-07 15:14:50 +0700

    adding api config

--- app/src/main/java/com/bangkit/application/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/bangkit/application/data/remote/retrofit/ApiConfig.kt
@@ -6,25 +6,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiConfig {
-    // TODO: change using actual apiconfig when service is available
-//    fun getApiService(): ApiService {
-//        val loggingInterceptor = HttpLoggingInterceptor()
-//            .setLevel(HttpLoggingInterceptor.Level.BODY)
-////        val authInterceptor = Interceptor { chain ->
-////            val req = chain.request()
-////            val requestHeaders = req.newBuilder()
-////                .addHeader("Authorization", "Bearer $token")
-////                .build()
-////            chain.proceed(requestHeaders)
-////        }
-//        val client: OkHttpClient = OkHttpClient.Builder()
-//            .addInterceptor(loggingInterceptor)
-//            .build()
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl("https://story-api.dicoding.dev/v1/")
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .client(client)
-//            .build()
-//        return retrofit.create(ApiService::class.java)
-//    }
+    fun getApiService(): ApiService {
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+//        val authInterceptor = Interceptor { chain ->
+//            val req = chain.request()
+//            val requestHeaders = req.newBuilder()
+//                .addHeader("Authorization", "Bearer $token")
+//                .build()
+//            chain.proceed(requestHeaders)
+//        }
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://capstone-dot-submission-mgce-dwikie.et.r.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
 }
