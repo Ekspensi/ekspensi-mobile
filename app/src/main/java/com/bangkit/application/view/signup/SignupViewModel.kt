24010efce9ff30039616dab7750745433bbfdb9e
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-08 03:03:34 +0700

    adding model for register and login

--- app/src/main/java/com/bangkit/application/view/signup/SignupViewModel.kt
+++ app/src/main/java/com/bangkit/application/view/signup/SignupViewModel.kt
@@ -2,7 +2,11 @@ package com.bangkit.application.view.signup
 
 import androidx.lifecycle.ViewModel
 import com.bangkit.application.data.UserRepository
+import com.bangkit.application.data.remote.response.RegisterResponse
 
 class SignupViewModel(private val repository: UserRepository) : ViewModel() {
     //TODO: for signup purpose
+    suspend fun register(username: String, phoneNum: String, email: String, password: String): RegisterResponse{
+        return repository.register(username, phoneNum, email, password)
+    }
 }
