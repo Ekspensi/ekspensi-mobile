1edb184621ce6eb4be1a9d38c5bd426256fc4d81
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-15 00:02:50 +0700

    adding main and history view model and history activity

---
+++ app/src/main/java/com/bangkit/application/view/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.bangkit.application.view.main
+
+import androidx.lifecycle.ViewModel
+import com.bangkit.application.data.UserRepository
+
+class MainViewModel(private val repository: UserRepository): ViewModel() {
+    // TODO: for main activity purpose
+}
